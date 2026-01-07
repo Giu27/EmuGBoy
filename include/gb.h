42c17331962ffbfcc045d0a88f1eef7cd2420576
Giu27 <Caruso.giuseppe09@hotmail.com>
2026-01-07 15:12:44 +0100

    Added a comment with a basic Memory Map

--- include/gb.h
+++ include/gb.h
@@ -4,6 +4,21 @@
 
 #define MEMORY_SIZE 0x10000
 
+/* GB Memory map
+0000 to 3FFF: 16 Kib ROM Bank 00
+4000 to 7FFF: 16 Kib ROM Bank 01-NN
+8000 to 9FFF: 8 Kib VRAM
+A000 to BFFF: 8 Kib External RAM
+C000 to CFFF: 4 Kib WRAM
+D000 to DFFF: 4 Kib WRAM
+E000 to FDFF: Echo RAM (Nintendo says use of this RAM area is prohibited. It's a mirror of C000 - DDFF)
+FE00 to FE9F: OAM
+FEA0 to FEFF: Not usable (Once again, Nintendo says this area is prohibited)
+FF00 to FF7F: IO Registers
+FF80 to FFFE: HRAM
+FFFF to FFFF: IE Register
+*/
+
 class Gb{
     public:
         Cpu cpu;
