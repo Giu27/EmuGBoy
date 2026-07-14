5f48c650822dbb36f1619d2d42fb079853e571e5
Giu27 <Caruso.giuseppe09@hotmail.com>
2026-07-14 22:24:06 +0200

    working mbc1... mostly

--- include/mbc.h
+++ include/mbc.h
@@ -9,32 +9,32 @@ class Mbc {
         std::vector<uint8_t> rom;
         std::vector<uint8_t> ram;
 
-        bool ramEnabled = false;
-        bool hasBattery = false;
+        bool ram_enabled = false;
+        bool has_battery = false;
 
-        Mbc(std::vector<uint8_t> romData, bool battery) : rom(std::move(romData)), hasBattery(battery) {
-            uint32_t ramSize = 0;
+        Mbc(std::vector<uint8_t> rom_data, bool battery) : rom(std::move(rom_data)), has_battery(battery) {
+            uint32_t ram_size = 0;
             switch (rom[0x0149]) {
-                case 0x01: ramSize = 0x800;   break; // 2 KB
-                case 0x02: ramSize = 0x2000;   break; // 8 KB
-                case 0x03: ramSize = 0x8000;  break; // 32 KB (4 banks of 8 KB)
-                case 0x04: ramSize = 0x20000; break; // 128 KB (16 banks of 8 KB)
-                case 0x05: ramSize = 0x10000; break; // 64 KB (8 banks of 8 KB)
-                default:   ramSize = 0;      break; 
+                case 0x01: ram_size = 0x800;   break; // 2 KB
+                case 0x02: ram_size = 0x2000;   break; // 8 KB
+                case 0x03: ram_size = 0x8000;  break; // 32 KB (4 banks of 8 KB)
+                case 0x04: ram_size = 0x20000; break; // 128 KB (16 banks of 8 KB)
+                case 0x05: ram_size = 0x10000; break; // 64 KB (8 banks of 8 KB)
+                default:   ram_size = 0;      break; 
             }
         
-            if (ramSize > 0) {
-                ram.resize(ramSize, 0xFF);
+            if (ram_size > 0) {
+                ram.resize(ram_size, 0xFF);
             }
 
         }
 
         virtual ~Mbc() = default;
 
-        virtual uint8_t readRom(uint16_t address) = 0;
-        virtual void writeRom(uint16_t address, uint8_t value) = 0;
-        virtual uint8_t readRam(uint16_t address) {return 0xFF;}
-        virtual void writeRam(uint16_t address, uint8_t value) {}
+        virtual uint8_t readRom(uint16_t addr) = 0;
+        virtual void writeRom(uint16_t addr, uint8_t value) = 0;
+        virtual uint8_t readRam(uint16_t addr) {return 0xFF;}
+        virtual void writeRam(uint16_t addr, uint8_t value) {}
 };
 
-std::unique_ptr<Mbc> init_mbc(std::vector<uint8_t> romData);
+std::unique_ptr<Mbc> init_mbc(std::vector<uint8_t> rom_data);
--- src/mbc.cpp
+++ src/mbc.cpp
@@ -3,13 +3,13 @@
 
 class Mbc0 : public Mbc {
     public:
-        Mbc0(std::vector<uint8_t> romData) : Mbc(std::move(romData), false) {}
+        Mbc0(std::vector<uint8_t> rom_data) : Mbc(std::move(rom_data), false) {}
 
-        uint8_t readRom(uint16_t address) override {
-            return rom[address];
+        uint8_t readRom(uint16_t addr) override {
+            return rom[addr];
         }
 
-        void writeRom(uint16_t address, uint8_t value) override {
+        void writeRom(uint16_t addr, uint8_t value) override {
 
         }
 };
@@ -20,38 +20,65 @@ class Mbc1 : public Mbc {
         uint8_t ram_bank = 0;
         uint8_t banking_mode = 0;
     public:
-        Mbc1(std::vector<uint8_t> romData, bool battery) : Mbc(std::move(romData), battery) {}
+        Mbc1(std::vector<uint8_t> rom_data, bool battery) : Mbc(std::move(rom_data), battery) {}
 
-        uint8_t readRom(uint16_t address) override {
+        uint8_t readRom(uint16_t addr) override {
+            if (addr <= 0x3FFF) {
+                return rom[addr];
+            }
 
+            uint8_t actual_bank = rom_bank;
+            if (banking_mode == 0) {
+                actual_bank |= (ram_bank << 5);
+            }
+
+            uint32_t real_addr = (actual_bank * 0x4000) + (addr - 0x4000);
+            return rom[real_addr % rom.size()];
         }
 
-        void writeRom(uint16_t address, uint8_t value) override {
-
+        void writeRom(uint16_t addr, uint8_t value) override {
+            if (addr <= 0x1FFF) {
+                ram_enabled = ((value & 0x0F) == 0xA);
+            } else if (addr >= 0x2000 && addr <= 0x3FFF) {
+                rom_bank = value & 0x1F; 
+                if (rom_bank == 0) rom_bank = 1; 
+            } else if (addr >= 0x4000 && addr <= 0x5FFF) {
+                ram_bank = value & 0x03; 
+            } else if (addr >= 0x6000 && addr <= 0x7FFF) {
+                banking_mode = value & 0x01; 
+            }
         }
 
-        uint8_t readRam(uint16_t address) override {
-
+        uint8_t readRam(uint16_t addr) override {
+            if (ram_enabled && !ram.empty()) {
+                uint8_t actual_ram_bank = (banking_mode == 1) ? ram_bank : 0;
+                uint32_t real_addr = (actual_ram_bank * 0x2000) + (addr - 0xA000);
+                return ram[real_addr % ram.size()];
+            } else return 0xFF;
         }
 
-        void writeRam(uint16_t address, uint8_t value) override {
-
+        void writeRam(uint16_t addr, uint8_t value) override {
+            if (ram_enabled&& !ram.empty()) {
+                uint8_t actual_ram_bank = (banking_mode == 1) ? ram_bank : 0;
+                uint32_t real_addr = (actual_ram_bank * 0x2000) + (addr - 0xA000);
+                ram[real_addr % ram.size()] = value;
+            }
         }
 };
 
-std::unique_ptr<Mbc> init_mbc(std::vector<uint8_t> romData) {
-    uint8_t cart_type = romData[0x0147];
+std::unique_ptr<Mbc> init_mbc(std::vector<uint8_t> rom_data) {
+    uint8_t cart_type = rom_data[0x0147];
     switch (cart_type) {
         case 0:
-            return std::make_unique<Mbc0>(std::move(romData));
+            return std::make_unique<Mbc0>(std::move(rom_data));
         case 1:
-            return std::make_unique<Mbc1>(std::move(romData), false);
+            return std::make_unique<Mbc1>(std::move(rom_data), false);
             break;
         case 3:
-            return std::make_unique<Mbc1>(std::move(romData), true);
+            return std::make_unique<Mbc1>(std::move(rom_data), true);
             break;
         default:
             std::cout<<"Missing Cartridge Type: "<<std::hex<<(int)cart_type;
     }
-    return std::make_unique<Mbc0>(std::move(romData));
+    return std::make_unique<Mbc0>(std::move(rom_data));
 }
